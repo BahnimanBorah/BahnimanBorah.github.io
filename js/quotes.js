026e0d35778fb613fae308f64fa06680b23cb0bb
Bahniman Borah <bahnimanborah.borah7@gmail.com>
2018-07-27 18:23:53 +0530

    Updates

--- js/quotes.js
+++ js/quotes.js
@@ -1,12 +1,12 @@
 var quotes = [
-    '"Success is most often achieved by those who don\'t know that failure is inevitable." - Coco Chanel',
-    '"Things work out best for those who make the best of how things work out."- John Wooden',
-    '"Courage is grace under pressure." - Ernest Hemingway',
-    '"Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning." - Albert Einstein',
-    '"Someone is sitting in the shade today because someone planted a tree a long time ago." - Warren Buffett',
-    '“The biggest risk is not taking any risk… In a world that’s changing really quickly, the only strategy that is guaranteed to fail is not taking risks."— Mark Zuckerberg'
+    '"Success is most often achieved by those who don\'t know that failure is inevitable." <h6 class="text-warning">- Coco Chanel</h6>',
+    '"Things work out best for those who make the best of how things work out."- John Wooden</h6>',
+    '"Courage is grace under pressure." <h6 class="text-warning">- Ernest Hemingway</h6>',
+    '"Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning." <h6 class="text-warning">- Albert Einstein</h6>',
+    '"Someone is sitting in the shade today because someone planted a tree a long time ago." <h6 class="text-warning">- Warren Buffett</h6>',
+    '“The biggest risk is not taking any risk… In a world that’s changing really quickly, the only strategy that is guaranteed to fail is not taking risks." <h6 class="text-warning">— Mark Zuckerberg</h6>'
 ];
 
 for(var i=0;i<quotes.length;i++){
-    document.write("<p class='lead small'>"+quotes[i]+"</p>")
+    document.write("<p class='text-dark lead' style='font-size:15px'>"+quotes[i]+"</p>")
 }
